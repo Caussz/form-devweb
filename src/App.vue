61b6e29ce1ff1ef4d224a05d146001726eaf479b
Your Name <you@example.com>
2023-04-06 19:13:16 -0300

    Inicio html e css

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,117 @@ const confirma = computed((nome, idade, dm, estado, cidade, endereco, hobbies, l
 
 <template>
 
+  <main class="container">
+    <h2>Form DevWeb</h2>
+
+    <form> 
+        <div class="input-field">
+            <input type="text" placeholder="Seu nome">
+            <div class="underline"></div>
+        </div>
+        <br>
+        <div class="input-field">
+            <input type="number" placeholder="Sua idade">
+            <div class="underline"></div>
+        </div>
+        <input type="submit" value="Cadastrar info">
+        <br>
+    </form>
+
+   
+</main>
+
 
 </template>
 
-<style scoped></style>
+<style scoped>
+  main.container {
+  background: rgba(194, 176, 176, 0.815);
+  min-width: 320px;
+  min-height: 40vh;
+  padding: 2rem;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+  }
+  
+  main h2 {
+  font-weight: 600;
+  margin-bottom: 2rem;
+  position: relative;
+  }
+  
+  form {
+  display: flex;
+  flex-direction: column;
+  }
+  
+  .input-field {
+  margin: 10px;
+  padding: 10px;
+  background-color: transparent;
+  position: relative;
+  }
+  
+  form .input-field:first-child {
+  margin-bottom: 1.5rem;
+  }
+  
+  .input-field .underline::before {
+  content: '';
+  position: absolute;
+  height: 1px;
+  width: 100%;
+  bottom: -5px;
+  left: 0;
+  background: rgba(0, 0, 0, 0.2);
+  }
+  
+  .input-field .underline::after {
+  content: '';
+  position: absolute;
+  height: 1px;
+  width: 100%;
+  bottom: -5px;
+  left: 0;
+  background: linear-gradient(45deg, #333135, #000000);
+  transform: scaleX(0);
+  transition: all .3s ease-in-out;
+  transform-origin: left;
+  }
+  
+  .input-field input:focus ~ .underline::after {
+  transform: scaleX(1);
+  }
+  
+  .input-field input {
+  background-color: transparent;
+  outline: none;
+  font-size: 0.9rem;
+  color: rgba(0, 0, 0, 0.7);
+  width: 100%;
+  }
+  
+  .input-field input::placeholder {
+  color: rgba(0, 0, 0, 0.5);
+  }
+  
+  form input[type="submit"] {
+  margin-top: 2rem;
+  padding: 0.4rem;
+  width: 100%;
+background-color: rgb(111, 86, 116);;
+  cursor: pointer;
+  color: white;
+  font-size: 0.9rem;
+  font-weight: 300;
+  border-radius: 4px;
+  transition: all 0.3s ease;
+  }
+  
+  form input[type="submit"]:hover {
+  transition: 5ms;
+  letter-spacing: 0.5px;
+  background: linear-gradient(to right, #5f6675, #537075);
+  }
+
+</style>
